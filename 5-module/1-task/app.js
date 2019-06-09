c92ee975820f04d827b575cb78046e1b8b7d2a54
Vika Petrenko <vpetrenko@mindk.com>
2019-06-09 20:13:02 +0300

    add solution for 5-module 1-task - chat

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,16 +1,33 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(require('koa-static')('public'));
+app.use(require('koa-static')(__dirname + '/public'));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
 
+let message = null;
+
+let clients = [];
+
 router.get('/subscribe', async (ctx, next) => {
+    await new Promise((resolve, reject) => {
+        clients.push(message => {
+            ctx.body = message;
+            resolve(message)
+        });
+    });
 });
 
 router.post('/publish', async (ctx, next) => {
+    if (ctx.request.body.message) {
+        clients.forEach(resolve => {
+            resolve(ctx.request.body.message);
+        });
+    }
+
+    ctx.body = message;
 });
 
 app.use(router.routes());
